f6077f3b66a097cc50ca8aaa6072d2b64c858fb2
Nha Le Truc <nha.letruc@gmail.com>
2024-01-12 15:07:20 +0700

    fix3 sts:AssumeRoleWithWebIdentity

--- .github/setup/main.tf
+++ .github/setup/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com:aud"
+  url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.github_actions_oidc_provider.certificates[0].sha1_fingerprint]
 }
